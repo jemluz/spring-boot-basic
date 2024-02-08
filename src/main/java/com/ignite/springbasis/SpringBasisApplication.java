873aeead6a1776d0101a553ef1757b22935b5336
jemluz <contatojemima@gmail.com>
2024-02-08 17:46:51 -0300

    add: create spring boot project

---
+++ src/main/java/com/ignite/springbasis/SpringBasisApplication.java
@@ -0,0 +1,13 @@
+package com.ignite.springbasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasisApplication.class, args);
+	}
+
+}
