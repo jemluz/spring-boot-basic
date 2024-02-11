9b670f1389aa15813cf0a2d18aebc8b77de3a07a
Jemima <contatojemima@gmail.com>
2024-02-11 11:38:08 -0300

    feature/spring basis (#3)
    * add: create spring boot project
    * fix: server no static resource (try to create again)
    
    closes #1

---
+++ src/test/java/com/ignite/springbasis/SpringBasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignite.springbasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
