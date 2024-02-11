12a74b2552fe60a777235b761f3a2b75ed5e460c
jemluz <contatojemima@gmail.com>
2024-02-11 11:40:05 -0300

    add: controller

---
+++ src/main/java/com/ignite/springbasis/Controller.java
@@ -0,0 +1,16 @@
+package com.ignite.springbasis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/recurso-inicial")
+public class Controller {
+
+  @GetMapping("/teste") // recurso final
+  public String getMethodName() {
+    return new String("getzinho bala");
+  }
+
+}
