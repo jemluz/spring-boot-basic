26884ac5a94dbfa1f1cddd7e92b367e23c3fe0aa
jemluz <contatojemima@gmail.com>
2024-02-12 08:26:34 -0300

    add: path and query params

--- src/main/java/com/ignite/springbasis/controller/Controller.java
+++ src/main/java/com/ignite/springbasis/controller/Controller.java
@@ -1,16 +1,31 @@
 package com.ignite.springbasis.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/recurso-inicial")
 public class Controller {
 
-  @GetMapping("/teste") // recurso final
-  public String getMethodName() {
-    return new String("getzinho bala");
+  @GetMapping("/teste-path/{id}") // recurso final
+  public String getPath(@PathVariable String id) {
+    return "getzinho path " + id;
+  }
+
+  @GetMapping("/teste-query") // recurso final
+  public String getQuery(@RequestParam String id) {
+    return "getzinho query " + id;
+  }
+
+  @GetMapping("/teste-querys") // recurso final
+  public String getQuerys(@RequestParam Map<String, String> allParams) {
+    return "getzinho query " + allParams.entrySet();
   }
 
 }
